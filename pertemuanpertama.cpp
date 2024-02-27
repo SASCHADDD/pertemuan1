066b3e7049d62cc746798bcebbb1c3d474db817b
SASCHADDD <saschadanu23@gmail.com>
2024-02-27 19:31:39 +0700

    first commit

---
+++ pertemuanpertama.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{ //begin
+float nJejari,nluas;
+cout <<"Masukan jejari";
+cin >> nJejari;
+nluas= 3.14*nJejari*nJejari;
+cout << "luas adalah =" <<nluas<<endl;
+}//end
